49c12b2b9b7a2ced3634725cbbba7ee06a531be2
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-03-23 12:59:31 +0300

    fix issues from reviewer

--- hack.py
+++ hack.py
@@ -1,7 +1,5 @@
 import random
 
-from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
-
 from datacenter.models import (
     Chastisement,
     Commendation,
@@ -12,33 +10,65 @@ from datacenter.models import (
 )
 
 
+COMMENDATIONS = [
+    'Молодец!', 
+	'Отлично!', 
+	'Хорошо!', 
+	'Гораздо лучше, чем я ожидал!',
+    'Ты меня приятно удивил!', 
+	'Великолепно!', 
+	'Прекрасно!',
+    'Очень хороший ответ!', 
+	'Талантливо!', 
+	'Уже существенно лучше!',
+    'Потрясающе!', 
+	'Замечательно!', 
+	'Прекрасное начало!',
+    'Так держать!', 
+	'Ты на правильном пути!', 
+	'Здорово!',
+    'Это как раз то, что нужно!', 
+	'Я тобой горжусь!',
+    'С каждым разом у тебя получается всё лучше!',
+    'Мы с тобой обязательно договоримся!',
+    'Я вижу, как ты стараешься!', 
+	'Ты растешь над собой!',
+    'Ты многое сделал, я это вижу!', 
+	'Теперь твои успехи станут заметными всем!',
+]
+
+
+class SchoolkidError(Exception):
+	pass
+
+
+class LessonNotFoundError(Exception):
+    pass
+
+
 def fix_marks(schoolkid_name):
 	marks = Mark.objects.filter(
 		schoolkid__full_name__contains=schoolkid_name, 
 		points__lt=4
-	)
-	for mark in marks:
-		mark.points = 5
-		mark.save()
+	).update(points=5)
 
 
 def remove_chastisements(schoolkid_name):
 	chastisements = Chastisement.objects.filter(
 		schoolkid__full_name__contains=schoolkid_name
-	)
-	for chastisement in chastisements:
-		chastisement.delete()
+	).delete()
 
 
-def create_commedation(schoolkid_name, subject_title):
+def create_commendation(schoolkid_name, subject_title):
 	try:
 		child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-	except ObjectDoesNotExist:
+	except Schoolkid.DoesNotExist:
 		print(f"Ошибка: Ученик '{schoolkid_name}' не найден.")
-		return
-	except MultipleObjectsReturned:
-		print(f"Ошибка: Найдено несколько учеников с именем '{schoolkid_name}'")
-		return
+		return None
+	except Schoolkid.MultipleObjectsReturned:
+		raise SchoolkidError(
+			f"Ошибка: Найдено несколько учеников с именем '{schoolkid_name}'"
+		)
 	
 	subject = Subject.objects.filter(
 		title__contains=subject_title,
@@ -58,35 +88,13 @@ def create_commedation(schoolkid_name, subject_title):
 
 	target_lesson = available_for_commendations.first()
 
-	commendation_variants = [
-		'Молодец!',
-        'Отлично!',
-        'Хорошо!',
-        'Гораздо лучше, чем я ожидал!',
-        'Ты меня приятно удивил!',
-        'Великолепно!',
-        'Прекрасно!',
-        'Очень хороший ответ!',
-        'Талантливо!',
-        'Уже существенно лучше!',
-        'Потрясающе!',
-        'Замечательно!',
-        'Прекрасное начало!',
-        'Так держать!',
-        'Ты на правильном пути!',
-        'Здорово!',
-        'Это как раз то, что нужно!',
-        'Я тобой горжусь!',
-        'С каждым разом у тебя получается всё лучше!',
-        'Мы с тобой обязательно договоримся!',
-        'Я вижу, как ты стараешься!',
-        'Ты растешь над собой!',
-        'Ты многое сделал, я это вижу!',
-        'Теперь твои успехи станут заметными всем!',
-	]
+	if not target_lesson:
+		raise LessonNotFoundError(
+			f"У ученика {child.full_name} нет уроков по предмету '{subject_title}'."
+		)
 	
 	Commendation.objects.create(
-		text=random.choice(commendation_variants),
+		text=random.choice(COMMENDATIONS),
 		created=target_lesson.date,
 		schoolkid=child,
 		subject=subject,
